74fc873daa0e08c6c8ea254741cc44a4503c2c8e
eldovyn <farras.pramudita@gmail.com>
2025-10-02 16:34:54 +0700

    feat: add home page

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Register from './views/auth/Register.vue'
 import Login from './views/auth/Login.vue'
+import Home from './views/Home.vue'
 
 const routes = [
     { path: '/register', component: Register },
     { path: '/login', component: Login },
+    { path: '/', component: Home },
 ]
 
 const router = createRouter({
