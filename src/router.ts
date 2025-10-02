a18771e09637b0fd46b13b22650515fec727d3bc
eldovyn <farras.pramudita@gmail.com>
2025-10-03 00:52:58 +0700

    feat: add page learning journey

--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,14 @@ import Register from './views/auth/Register.vue'
 import Login from './views/auth/Login.vue'
 import Home from './views/Home.vue'
 import JalurBelajar from './views/Jalur-Belajar.vue'
+import LearningJourney from './views/Learning-Journey.vue'
 
 const routes = [
     { path: '/register', component: Register },
     { path: '/login', component: Login },
     { path: '/', component: Home },
     { path: '/jalur-belajar', component: JalurBelajar },
+    { path: '/learning-journey', component: LearningJourney },
 ]
 
 const router = createRouter({
---
+++ src/views/Learning-Journey.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section class="recomendation ps-9.5 pt-15">
+        <div class="flex flex-row items-center pb-3 gap-2">
+            <h1 class="text-2xl font-bold">Mentor Favorit Nusaverse</h1>
+            <p class="text-[#5476FF]">Lihat Semua</p>
+        </div>
+        <div class="flex flex-row items-center gap-5">
+            <Card class="w-50 p-0 border-none shadow-none">
+                <CardHeader class="ps-2 pe-2 justify-center">
+                    <img src="/Ellipse 21.png" class="h-20 w-20" alt="">
+                </CardHeader>
+                <CardFooter class="ps-2 pe-2 flex flex-col gap-1 justify-center items-center text-center">
+                    <h1 class="text-[15px] font-bold text-center">Sonny Philo</h1>
+                    IT Project Manager @ Pertamina Oil & Gas
+                </CardFooter>
+            </Card>
+            <Card class="w-50 p-0 border-none shadow-none">
+                <CardHeader class="ps-2 pe-2 justify-center">
+                    <img src="/Ellipse 21.png" class="h-20 w-20" alt="">
+                </CardHeader>
+                <CardFooter class="ps-2 pe-2 flex flex-col gap-1 justify-center items-center text-center">
+                    <h1 class="text-[15px] font-bold text-center">Sonny Philo</h1>
+                    IT Project Manager @ Pertamina Oil & Gas
+                </CardFooter>
+            </Card>
+            <Card class="w-50 p-0 border-none shadow-none">
+                <CardHeader class="ps-2 pe-2 justify-center">
+                    <img src="/Ellipse 21.png" class="h-20 w-20" alt="">
+                </CardHeader>
+                <CardFooter class="ps-2 pe-2 flex flex-col gap-1 justify-center items-center text-center">
+                    <h1 class="text-[15px] font-bold text-center">Sonny Philo</h1>
+                    IT Project Manager @ Pertamina Oil & Gas
+                </CardFooter>
+            </Card>
+            <Card class="w-50 p-0 border-none shadow-none">
+                <CardHeader class="ps-2 pe-2 justify-center">
+                    <img src="/Ellipse 21.png" class="h-20 w-20" alt="">
+                </CardHeader>
+                <CardFooter class="ps-2 pe-2 flex flex-col gap-1 justify-center items-center text-center">
+                    <h1 class="text-[15px] font-bold text-center">Sonny Philo</h1>
+                    IT Project Manager @ Pertamina Oil & Gas
+                </CardFooter>
+            </Card>
+            <Card class="w-50 p-0 border-none shadow-none">
+                <CardHeader class="ps-2 pe-2 justify-center">
+                    <img src="/Ellipse 21.png" class="h-20 w-20" alt="">
+                </CardHeader>
+                <CardFooter class="ps-2 pe-2 flex flex-col gap-1 justify-center items-center text-center">
+                    <h1 class="text-[15px] font-bold text-center">Sonny Philo</h1>
+                    IT Project Manager @ Pertamina Oil & Gas
+                </CardFooter>
+            </Card>
+            <Card class="w-50 p-0 border-none shadow-none">
+                <CardHeader class="ps-2 pe-2 justify-center">
+                    <img src="/Ellipse 21.png" class="h-20 w-20" alt="">
+                </CardHeader>
+                <CardFooter class="ps-2 pe-2 flex flex-col gap-1 justify-center items-center text-center">
+                    <h1 class="text-[15px] font-bold text-center">Sonny Philo</h1>
+                    IT Project Manager @ Pertamina Oil & Gas
+                </CardFooter>
+            </Card>
+        </div>
+    </section>
+</template>
