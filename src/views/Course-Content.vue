bf1e38e28e35647fe3dad38eb04f64b20236e266
eldovyn <farras.pramudita@gmail.com>
2025-10-04 01:54:47 +0700

    feat: add course content

--- src/views/Course-Content.vue
+++ src/views/Course-Content.vue
@@ -10,7 +10,7 @@ import {
 
 <template>
     <div class="flex min-h-screen">
-        <div class="w-1/2 p-8">
+        <div class="w-1/2 p-8 overflow-y-auto max-h-screen hide-scrollbar">
             <img src="/Rectangle 268.png" alt="Roadmap" class="w-full h-auto rounded-lg mb-6">
             <h1 class="text-[35.3px] font-bold mb-4">Building Product Roadmaps With Confidence</h1>
             <div class="flex justify-between items-center">
@@ -35,14 +35,14 @@ import {
                 dengan kebutuhan pasar. Kamu akan mempelajari prinsip dasar perencanaan produk, teknik menentukan
                 prioritas fitur dengan metode seperti RICE atau MoSCoW, serta cara menghubungkan roadmap dengan visi dan
                 strategi bisnis. Selain itu, kursus ini juga membekali kamu dengan keterampilan komunikasi agar roadmap
-                yang dibuat dapat dipresentasikan dengan percaya diri kepada stakeholder maupun tim lintas fungsi.
-
-                Melalui kombinasi teori, studi kasus, dan latihan praktis, kamu akan memperoleh pengalaman langsung
+                yang dibuat dapat dipresentasikan dengan percaya diri kepada stakeholder maupun tim lintas fungsi.</p>
+            <p class="mt-4">Melalui kombinasi teori, studi kasus, dan latihan praktis, kamu akan memperoleh pengalaman
+                langsung
                 merancang roadmap yang tidak hanya realistis tetapi juga mampu menjadi panduan strategis dalam
                 mengarahkan pengembangan produk jangka panjang. Pada akhir kursus, kamu akan lebih siap menghadapi
                 tantangan product management sekaligus meningkatkan peluang keberhasilan produk di pasar.</p>
 
-            <Card class="w-80 border border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)]">
+            <Card class="w-95 border border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] mt-5">
                 <CardHeader class="ps-2 pe-2">
                     <CardTitle class="text-[18px] font-bold">
                         Kursus Ini Mencakup
@@ -53,21 +53,53 @@ import {
                     <img src="/Frame 1618869427.png" alt="">
                 </CardContent>
             </Card>
+
+            <Card class="w-95 border border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] mt-5 h-45">
+                <CardHeader class="ps-2 pe-2 mb-0">
+                    <CardTitle class="text-[18px] font-bold">
+                        Kursus Ini Mencakup
+                    </CardTitle>
+                </CardHeader>
+
+                <CardContent class="ps-2 pe-2 flex flex-row items-center gap-2 mt-0">
+                    <img src="/Frame 1618869444.png" alt="">
+                </CardContent>
+            </Card>
         </div>
 
-        <div class="w-1/2 p-8 bg-white">
-            <h2 class="text-xl font-semibold mb-4">Section 1 - Fundamentals</h2>
-            <ul class="space-y-3">
-                <li class="p-3 bg-white rounded shadow">
-                    <span>1. Peran Roadmap dalam Siklus Hidup Produk</span>
-                </li>
-                <li class="p-3 bg-white rounded shadow">
-                    <span>2. Visi Produk: Pondasi Roadmap</span>
-                </li>
-                <li class="p-3 bg-white rounded shadow">
-                    <span>3. Menentukan Tujuan dan Outcome Bisnis</span>
-                </li>
-            </ul>
+        <div class="w-1/2 p-8 bg-white overflow-y-auto max-h-screen hide-scrollbar">
+            <Button class="w-[119px] text-[20px] h-12 bg-[#5476FF] me-3">Section</Button>
+            <Button
+                class="w-[119px] text-[20px] h-12 bg-transparent text-[#5476FF] rounded-lg border-1 ms-3 border-[#5476FF]">Project</Button>
+            <img src="/Frame 1618869490.png" oncontextmenu="return false;" class="mt-5" alt="">
+            <img src="/Frame 1618869496.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869498.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869499.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869500.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869501.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869502.png" oncontextmenu="return false;" class="mt-5" alt="">
+            <img src="/Frame 1618869503.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869510.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869508.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869509.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869511.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869502.png" oncontextmenu="return false;" class="mt-5" alt="">
+            <img src="/Frame 1618869513.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869514.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869515.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869516.png" oncontextmenu="return false;" alt="">
+            <img src="/Frame 1618869517.png" oncontextmenu="return false;" alt="">
         </div>
     </div>
 </template>
+
+<style>
+.hide-scrollbar::-webkit-scrollbar {
+    display: none;
+}
+
+.hide-scrollbar {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+}
+</style>
