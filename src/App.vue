9c93b90fcd3cde17bff27a0b8569762118f28064
eldovyn <farras.pramudita@gmail.com>
2025-10-03 23:02:36 +0700

    fix(login/register): remove navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
 import { useRoute } from 'vue-router'
+import { computed } from 'vue'
 
 const route = useRoute()
-const showNavbar = !['/login', '/register'].includes(route.path)
+const showNavbar = computed(() => !['/login', '/register'].includes(route.path))
 </script>
 
 <template>
   <NavBar v-if="showNavbar" />
   <RouterView />
 </template>
-
-<style scoped></style>
