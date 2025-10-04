c67c8814e1c710f93b44f6a0774adfa168c079e6
eldovyn <farras.pramudita@gmail.com>
2025-10-04 16:14:27 +0700

    style(submit-latihan): update submit latihan

--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,14 @@ import Register from './views/auth/Register.vue'
 import Login from './views/auth/Login.vue'
 import CourseContent from './views/Course-Content.vue'
 import ProfileMentor from './views/Profile-Mentor.vue'
+import SubmitLatihan from './views/Submit-Latihan.vue'
 
 const routes = [
     { path: '/register', component: Register },
     { path: '/login', component: Login },
     { path: '/course-content', component: CourseContent },
     { path: '/profile-mentor', component: ProfileMentor },
+    { path: '/submit-latihan', component: SubmitLatihan },
 ]
 
 const router = createRouter({
