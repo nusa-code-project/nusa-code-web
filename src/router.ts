3b9fb498710c66c916d3580823f9f7ec422549ba
eldovyn <farras.pramudita@gmail.com>
2025-10-04 09:38:35 +0700

    feat: add page profile mentor

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Register from './views/auth/Register.vue'
 import Login from './views/auth/Login.vue'
 import CourseContent from './views/Course-Content.vue'
+import ProfileMentor from './views/Profile-Mentor.vue'
 
 const routes = [
     { path: '/register', component: Register },
     { path: '/login', component: Login },
     { path: '/course-content', component: CourseContent },
+    { path: '/profile-mentor', component: ProfileMentor },
 ]
 
 const router = createRouter({
---
+++ src/views/Profile-Mentor.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h2>Profile Mentor</h2>
+</template>
