5e504aafa05f2aa091441d91b3649de69d69b80e
eldovyn <farras.pramudita@gmail.com>
2025-10-12 11:10:06 +0700

    feat: add page ai-learning

--- src/views/Ai-Learning.vue
+++ src/views/Ai-Learning.vue
@@ -3,49 +3,54 @@ import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-    <div class="h-screen flex justify-center items-center w-full">
-        <div class="flex justify-center items-center gap-20">
-            <div
-                class="relative bg-[#E6EBFF40] rounded-md w-[480px] p-10 shadow-[0px_4px_4px_rgba(230,235,255,0.25)] overflow-hidden">
+    <div class="max-h-screen w-full flex justify-center overflow-hidden">
+        <div class="min-h-screen flex justify-center items-center">
+            <div class="flex justify-center items-center gap-20">
+                <div
+                    class="relative bg-[#FFFFFF] w-[480px] p-10 border border-[#E6EBFF] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] rounded-lg">
+                    <img src="/mingcute_ai-fill.png" alt="background"
+                        class="absolute left-1/2 top-1/2 w-[200px] h-[200px] -translate-x-1/2 -translate-y-1/2 object-contain opacity-40 mix-blend-multiply z-0" />
 
-                <img src="/mingcute_ai-fill.png" alt="background"
-                    class="absolute inset-0 w-full h-full object-contain opacity-40 mix-blend-multiply z-0" />
-
-                <div class="relative z-10">
-                    <h1 class="text-center text-2xl font-bold">AI Learning Path Assessment</h1>
-                    <br><br>
-                    <p class="text-center">
-                        Pengen terjun ke dunia IT tapi bingung milih spesialisasi? Sini aku spill jalur
-                        yang paling pas buat kamu! Cukup jawab beberapa pertanyaan santai, aku jadi bisa ngebaca potensi
-                        dan
-                        minatmu ngarahnya ke mana. Data science? UI/UX? Atau yang lain? Setelah itu, auto dapet
-                        rekomendasi roadmap belajar yang
-                        paling cocok, biar perjalanan karirmu di dunia IT makin mulus!
-                    </p>
-                    <br>
-                    <div class="flex justify-center gap-5">
-                        <Button class="bg-[#5476FF] hover:bg-[#5476FF] w-35">
-                            Mulai Assesment
-                        </Button>
-                        <Button class="border bg-transparent text-[#5476FF] hover:bg-transparent border-[#5476FF] w-35">
-                            <img src="/icon-left (2).png" alt="">
-                            <p>15 Soal</p>
-                        </Button>
+                    <div class="relative z-10">
+                        <h1 class="text-center text-2xl font-bold mb-5">
+                            AI Learning Path Assessment
+                        </h1>
+                        <p class="text-center mb-5">
+                            Pengen terjun ke dunia IT tapi bingung milih spesialisasi? Sini aku
+                            spill jalur yang paling pas buat kamu! Cukup jawab beberapa
+                            pertanyaan santai, aku jadi bisa ngebaca potensi dan minatmu
+                            ngarahnya ke mana. Data science? UI/UX? Atau yang lain? Setelah itu,
+                            auto dapet rekomendasi roadmap belajar yang paling cocok, biar
+                            perjalanan karirmu di dunia IT makin mulus!
+                        </p>
+                        <div class="flex justify-center gap-5">
+                            <Button class="bg-[#5476FF] hover:bg-[#5476FF] w-35">
+                                Mulai Assesment
+                            </Button>
+                            <Button
+                                class="border bg-transparent text-[#5476FF] hover:bg-transparent border-[#5476FF] w-35">
+                                <img src="/icon-left (2).png" alt="" />
+                                <p>15 Soal</p>
+                            </Button>
+                        </div>
                     </div>
-                    <br>
                 </div>
-            </div>
 
-            <div class="bg-[#E6EBFF40] w-[480px] p-5 shadow-[0px_4px_4px_rgba(230,235,255,0.25)]">
-                <p>
-                    Pengen terjun ke dunia IT tapi bingung milih spesialisasi? Sini aku spill jalur yang paling pas buat
-                    kamu! Cukup jawab beberapa pertanyaan santai, aku jadi bisa ngebaca potensi dan minatmu ngarahnya ke
-                    mana.
-                    Data science? UI/UX? Atau yang lain? Setelah itu, auto dapet rekomendasi roadmap belajar yang paling
-                    cocok,
-                    biar perjalanan karirmu di dunia IT makin mulus!
-                </p>
+                <div class="relative flex w-[600px] h-[390px]">
+                    <div class="absolute top-0 left-0 w-[300px] h-[80px] p-4 border border-[#E6EBFF]
+           bg-white shadow-[0_4px_4px_0_rgba(230,235,255,0.25)]
+           rounded-lg">
+                        <p>
+                            Berikan jawaban sesuai dengan apa yang kamu anggap terbaik ya!
+                        </p>
+                    </div>
+
+                    <img src="/Mascot.png" alt="Mascot"
+                        class="absolute inset-0 m-auto w-120 translate-y-[60px] translate-x-[30px]" />
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped></style>
