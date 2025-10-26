f78d0ef2e9f46da3f5a649250bf4b5e9bb7f69ad
eldovyn <farras.pramudita@gmail.com>
2025-10-26 23:43:56 +0700

    feat(roadmap): add section roadmap web dev

--- src/views/Roadmap.vue
+++ src/views/Roadmap.vue
@@ -21,7 +21,7 @@
         <img src="/Jalur Belajar - Materi per tahapan 1.png" alt="">
     </section>
 
-    <!-- <section class="kursus-favorite flex flex-col ps-10 pt-10 gap-5">
+    <section class="kursus-favorite flex flex-col ps-10 pt-10 gap-5">
         <div class="flex flex-row items-center gap-2">
             <h2 class="inline-block bg-clip-text text-transparent font-bold text-2xl mx-auto" style="background: linear-gradient(270deg, #977dff 0%, #002ee8 100%);
            -webkit-text-fill-color: transparent;
@@ -30,5 +30,75 @@
                 Web Development
             </h2>
         </div>
-    </section> -->
+
+        <div class="flex justify-center gap-10">
+            <div
+                class="border-3 border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] p-3 rounded-lg flex flex-col gap-2">
+                <h3 class="text-xl font-bold">Front-End Developer</h3>
+                <p class="text-sm" style="
+            display:-webkit-box;
+            -webkit-box-orient:vertical;
+            max-width:40ch; 
+        ">Peta jalan ini menjelaskan tahapan belajar menjadi Front-End Developer, dari dasar
+                    hingga mahir
+                    membangun antarmuka pengguna yang interaktif.</p>
+            </div>
+            <div
+                class="border-3 border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] p-3 rounded-lg flex flex-col gap-2">
+                <h3 class="text-xl font-bold">Back-End Developer</h3>
+                <p class="text-sm" style="
+            display:-webkit-box;
+            -webkit-box-orient:vertical;
+            max-width:40ch; 
+        ">Peta jalan ini menjelaskan tahapan belajar menjadi Back-End Developer, dari dasar
+                    hingga mahir
+                    membangun antarmuka pengguna yang interaktif.</p>
+            </div>
+            <div
+                class="border-3 border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] p-3 rounded-lg flex flex-col gap-2">
+                <h3 class="text-xl font-bold">Full-Stack Developer</h3>
+                <p class="text-sm" style="
+            display:-webkit-box;
+            -webkit-box-orient:vertical;
+            max-width:40ch; 
+        ">Peta jalan ini menjelaskan tahapan belajar menjadi Full-Stack Developer, dari fondasi dasar hingga mahir
+                    mengelola seluruh sisi pengembangan web.</p>
+            </div>
+        </div>
+
+        <div class="flex justify-center gap-10">
+            <div
+                class="border-3 border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] p-3 rounded-lg flex flex-col gap-2">
+                <h3 class="text-xl font-bold">DevOps Engineer</h3>
+                <p class="text-sm" style="
+            display:-webkit-box;
+            -webkit-box-orient:vertical;
+            max-width:40ch; 
+        ">Peta jalan ini menjelaskan tahapan belajar menjadi DevOps Engineer, dari dasar hingga mahir mengotomatiskan
+                    proses deployment.</p>
+            </div>
+            <div
+                class="border-3 border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] p-3 rounded-lg flex flex-col gap-2">
+                <h3 class="text-xl font-bold">QA (Quality Assurance)</h3>
+                <p class="text-sm" style="
+            display:-webkit-box;
+            -webkit-box-orient:vertical;
+            max-width:40ch; 
+        ">Peta jalan ini menjelaskan tahapan belajar menjadi QA Engineer, dari dasar testing hingga ahli dalam menjaga
+                    kualitas perangkat lunak.</p>
+            </div>
+            <div
+                class="border-3 border-[#e6ebff] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] p-3 rounded-lg flex flex-col gap-2">
+                <h3 class="text-xl font-bold">Web Maintenance</h3>
+                <p class="text-sm" style="
+            display:-webkit-box;
+            -webkit-box-orient:vertical;
+            max-width:40ch; 
+        ">Peta jalan ini menjelaskan tahapan belajar menjadi Web Maintenance, dari dasar hingga ahli dalam memelihara
+                    performa dan keamanan website.</p>
+            </div>
+        </div>
+    </section>
+    <br>
+    <br>
 </template>
