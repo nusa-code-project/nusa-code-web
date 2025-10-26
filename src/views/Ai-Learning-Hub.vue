2bd0110fc9600e7f4abdea3948d8ab74b2efbb3d
eldovyn <farras.pramudita@gmail.com>
2025-10-26 20:32:29 +0700

    feat(ai-learning-hub): add section ai role play

--- src/views/Ai-Learning-Hub.vue
+++ src/views/Ai-Learning-Hub.vue
@@ -1,5 +1,34 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import { ref } from 'vue'
+
+
+const moduleAiRolePlay = ref([
+    {
+        thumbnail: '/Rectangle 39 (8).png',
+        title: 'Negosiasi Deadline yang Dimajukan dengan Project Manager',
+        duration: '5 menit',
+        category: 'Easy',
+    },
+    {
+        thumbnail: '/Rectangle 39 (9).png',
+        title: 'Memberikan Feedback Konstruktif ke Rekan Tim Pengembangan',
+        duration: '5 menit',
+        category: 'Easy',
+    },
+    {
+        thumbnail: '/Rectangle 39 (10).png',
+        title: 'Simulasi Daily Standup: Melaporkan Progres dengan Jelas',
+        duration: '5 menit',
+        category: 'Easy',
+    },
+    {
+        thumbnail: '/Rectangle 39 (11).png',
+        title: 'Simulasi Daily Standup: Melaporkan Progres dengan Jelas',
+        duration: '5 menit',
+        category: 'Easy',
+    }
+])
 </script>
 
 <template>
@@ -65,5 +94,122 @@ import { Button } from '@/components/ui/button'
               absolute right-0 top-0
               w-24 sm:w-32 md:w-40 h-auto
               -z-10">
+    </section>
+
+    <section class="ai-role-play mt-10 ms-20">
+        <div class="flex gap-5 items-center">
+            <h1 class="text-2xl font-bold">Interpersonal & Communication</h1>
+            <p class="text-sm text-[#5476ff]">Lihat Semua</p>
+        </div>
+        <div class="flex flex-row gap-5 mt-5">
+            <div v-for="(card, idx) in moduleAiRolePlay" :key="idx"
+                class="border border-[#E6EBFF] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] w-90 p-3 rounded-xl flex flex-col">
+                <img :src="card.thumbnail" alt="" class="rounded-lg w-full object-cover h-50" />
+
+                <h1 class="text-2xl font-semibold mt-3 min-h-[60px] line-clamp-2 mb-1">
+                    {{ card.title }}
+                </h1>
+
+                <div class="flex mt-2 items-center gap-2">
+                    <img src="/fi-br-time-fast.png" alt="">
+                    <p>{{ card.duration }}</p>
+                </div>
+
+                <div class="flex mt-2 items-center gap-2">
+                    <img src="/fi-br-signal-alt.png" alt="">
+                    <p>{{ card.duration }}</p>
+                </div>
+
+                <div class="flex justify-between mt-5">
+                    <Button
+                        class="w-1/3 flex justify-center items-center bg-transparent border border-[#5476FF] hover:bg-transparent">
+                        <img src="/fi-br-bookmark.png" alt="">
+                        <p class="text-[#5476FF] font-semibold">Simpan</p>
+                    </Button>
+                    <Button
+                        class="w-50 flex justify-center items-center bg-[#5476FF] border border-[#5476FF] hover:bg-[#5476FF]">
+                        Mulai Role Play
+                    </Button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ai-role-play mt-10 ms-20">
+        <div class="flex gap-5 items-center">
+            <h1 class="text-2xl font-bold">Problem-solving & Critical thinking</h1>
+            <p class="text-sm text-[#5476ff]">Lihat Semua</p>
+        </div>
+        <div class="flex flex-row gap-5 mt-5">
+            <div v-for="(card, idx) in moduleAiRolePlay" :key="idx"
+                class="border border-[#E6EBFF] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] w-90 p-3 rounded-xl flex flex-col">
+                <img :src="card.thumbnail" alt="" class="rounded-lg w-full object-cover h-50" />
+
+                <h1 class="text-2xl font-semibold mt-3 min-h-[60px] line-clamp-2 mb-1">
+                    {{ card.title }}
+                </h1>
+
+                <div class="flex mt-2 items-center gap-2">
+                    <img src="/fi-br-time-fast.png" alt="">
+                    <p>{{ card.duration }}</p>
+                </div>
+
+                <div class="flex mt-2 items-center gap-2">
+                    <img src="/fi-br-signal-alt.png" alt="">
+                    <p>{{ card.duration }}</p>
+                </div>
+
+                <div class="flex justify-between mt-5">
+                    <Button
+                        class="w-1/3 flex justify-center items-center bg-transparent border border-[#5476FF] hover:bg-transparent">
+                        <img src="/fi-br-bookmark.png" alt="">
+                        <p class="text-[#5476FF] font-semibold">Simpan</p>
+                    </Button>
+                    <Button
+                        class="w-50 flex justify-center items-center bg-[#5476FF] border border-[#5476FF] hover:bg-[#5476FF]">
+                        Mulai Role Play
+                    </Button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ai-role-play mt-10 ms-20">
+        <div class="flex gap-5 items-center">
+            <h1 class="text-2xl font-bold">Leadership & Growth Mindset</h1>
+            <p class="text-sm text-[#5476ff]">Lihat Semua</p>
+        </div>
+        <div class="flex flex-row gap-5 mt-5">
+            <div v-for="(card, idx) in moduleAiRolePlay" :key="idx"
+                class="border border-[#E6EBFF] shadow-[0_4px_4px_0_rgba(230,235,255,0.25)] w-90 p-3 rounded-xl flex flex-col">
+                <img :src="card.thumbnail" alt="" class="rounded-lg w-full object-cover h-50" />
+
+                <h1 class="text-2xl font-semibold mt-3 min-h-[60px] line-clamp-2 mb-1">
+                    {{ card.title }}
+                </h1>
+
+                <div class="flex mt-2 items-center gap-2">
+                    <img src="/fi-br-time-fast.png" alt="">
+                    <p>{{ card.duration }}</p>
+                </div>
+
+                <div class="flex mt-2 items-center gap-2">
+                    <img src="/fi-br-signal-alt.png" alt="">
+                    <p>{{ card.duration }}</p>
+                </div>
+
+                <div class="flex justify-between mt-5">
+                    <Button
+                        class="w-1/3 flex justify-center items-center bg-transparent border border-[#5476FF] hover:bg-transparent">
+                        <img src="/fi-br-bookmark.png" alt="">
+                        <p class="text-[#5476FF] font-semibold">Simpan</p>
+                    </Button>
+                    <Button
+                        class="w-50 flex justify-center items-center bg-[#5476FF] border border-[#5476FF] hover:bg-[#5476FF]">
+                        Mulai Role Play
+                    </Button>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
