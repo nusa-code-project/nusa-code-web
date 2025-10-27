4276bbabf6328400331e2949863de8b085133251
eldovyn <farras.pramudita@gmail.com>
2025-10-27 22:18:09 +0700

    feat: add page ai learning assesment

--- src/router.ts
+++ src/router.ts
@@ -12,6 +12,7 @@ import Testing from './views/Testing.vue'
 import Home from './views/Home.vue'
 import AiLearningHub from './views/Ai-Learning-Hub.vue'
 import Roadmap from './views/Roadmap.vue'
+import AiLearningAssesment from './views/Ai-Learning-Assesment.vue'
 
 const routes = [
     { path: '/register', component: Register },
@@ -27,6 +28,7 @@ const routes = [
     { path: '/', component: Home },
     { path: '/ai-learning-hub', component: AiLearningHub },
     { path: '/roadmap', component: Roadmap },
+    { path: '/ai-learning-assesment', component: AiLearningAssesment },
 ]
 
 const router = createRouter({
