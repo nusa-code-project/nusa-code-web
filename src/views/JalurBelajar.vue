9eed52fb01663df3bc1c989b36818a6028ce9524
eldovyn <farras.pramudita@gmail.com>
2025-11-01 00:16:05 +0700

    feat: add jalur belajar page

--- src/views/JalurBelajar.vue
+++ src/views/JalurBelajar.vue
@@ -1,8 +1,42 @@
 <script setup lang="ts">
+import Separator from '@/components/ui/separator/Separator.vue';
 </script>
 
 <template>
-    <p>Jalur Belajar</p>
+    <section class="jumbotron pt-40 flex flex-col gap-5">
+        <h1 class="text-[30px] text-center font-bold">Roadmap Belajar Menjadi Product Manager</h1>
+        <Separator class="py-1 !w-25 mx-auto rounded-md bg-[#5476ff]" />
+        <p class="font-semibold text-center text-[20px]">End of Learning</p>
+        <div class="flex flex-row mx-auto">
+            <div class="bg-[#f5f2ff] rounded-md flex items-center justify-center w-28">
+                <div class="flex flex-col">
+                    <p class="text-center font-semibold">90</p>
+                    <p class="text-center font-semibold">Days</p>
+                </div>
+            </div>
+            <p class="font-bold text-[100px]">:</p>
+            <div class="bg-[#f5f2ff] rounded-md flex items-center justify-center w-28">
+                <div class="flex flex-col">
+                    <p class="text-center font-semibold">03</p>
+                    <p class="text-center font-semibold">Hours</p>
+                </div>
+            </div>
+            <p class="font-bold text-[100px]">:</p>
+            <div class="bg-[#f5f2ff] rounded-md flex items-center justify-center w-28">
+                <div class="flex flex-col">
+                    <p class="text-center font-semibold">40</p>
+                    <p class="text-center font-semibold">Minutes</p>
+                </div>
+            </div>
+            <p class="font-bold text-[100px]">:</p>
+            <div class="bg-[#f5f2ff] rounded-md flex items-center justify-center w-28">
+                <div class="flex flex-col">
+                    <p class="text-center font-semibold">15</p>
+                    <p class="text-center font-semibold">Seconds</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped></style>
