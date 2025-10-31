8b52b7930f934a32fc33035a1e2edcd0b9e25a2c
eldovyn <farras.pramudita@gmail.com>
2025-11-01 00:24:22 +0700

    style(jalur-belajar): update font size jumbotron

--- src/views/JalurBelajar.vue
+++ src/views/JalurBelajar.vue
@@ -7,7 +7,7 @@ import Separator from '@/components/ui/separator/Separator.vue';
         <h1 class="text-[30px] text-center font-bold">Roadmap Belajar Menjadi Product Manager</h1>
         <Separator class="py-1 !w-25 mx-auto rounded-md bg-[#5476ff]" />
         <p class="font-semibold text-center text-[20px]">End of Learning</p>
-        <div class="flex flex-row mx-auto">
+        <div class="flex flex-row mx-auto gap-5 text-xl">
             <div class="bg-[#f5f2ff] rounded-md flex items-center justify-center w-28">
                 <div class="flex flex-col">
                     <p class="text-center font-semibold">90</p>
